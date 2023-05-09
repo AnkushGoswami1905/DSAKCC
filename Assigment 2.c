aa427908fb559949287caa1412fee27888ba3119
AnkushGoswami1905 <73483414+AnkushGoswami1905@users.noreply.github.com>
2023-05-09 13:07:50 +0530

    Create Assigment 2.c

---
+++ Assigment 2.c
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int s,n,m,sum;
+    cin>> s >> n >> m;
+    sum=s+n+m;
+    cout<<sum<<endl; 
+    return 0;
+}
